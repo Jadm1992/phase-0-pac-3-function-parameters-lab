b229f0551e40902be8dc82f030fb952a9d0c3c59
jadm1992 <devyonmiller8@gmail.com>
2022-06-15 12:50:57 -0400

    commit tree 1695d84909ab3692e1e8043f3982d6501308189f

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction (Akai) {
+    return `Hi, my name is ${Akai}.`
+}
+
+function introductionWithLanguage(name = "Akai", language = "Ember.js.") {
+return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+function introductionWithLanguageOptional(name = "Samip", language = "React.") {
+return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+function introductionWithLanguageOptional(name = "Gracie", language = "JavaScript") {
+return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+}
+// function introductionWithLanguageOptional(name = "Gracie", language = "Python") {
+// return (`Hi, my name is ${name} and I am learning to program in ${language}.`)
+// }
